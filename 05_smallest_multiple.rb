8a10e916d446e3f3e25ae2ff8aa93a6bee53a938
funwithcthulhu <thomasboeding@gmail.com>
2017-08-19 12:03:29 -0500

    finished 05

--- 05_smallest_multiple.rb
+++ 05_smallest_multiple.rb
@@ -1,4 +1,15 @@
-require 'Prime'
+=begin
+2520 is the smallest number that can be divided by each of
+the numbers from 1 to 10 without any remainder.
 
+What is the smallest positive number that is evenly divisible
+by all of the numbers from 1 to 20?
+=end
 def smallest_multiple(n)
-  (2..n).collect { |x| x.prime? }
+  start = n
+  num = start
+  loop do
+    (n % num).zero? ? num -= 1 : num = start
+    num.zero? (return n + 1) : (n += 1)
+  end
+end
