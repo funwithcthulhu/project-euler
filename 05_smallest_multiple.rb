df8ee1a983ab846bdf8d72d283d5c7b4e0cbd8e2
funwithcthulhu <thomasboeding@gmail.com>
2017-08-19 12:10:55 -0500

    refactored sol for 05

--- 05_smallest_multiple.rb
+++ 05_smallest_multiple.rb
@@ -10,6 +10,20 @@ def smallest_multiple(n)
   num = start
   loop do
     (n % num).zero? ? num -= 1 : num = start
-    num.zero? (return n + 1) : (n += 1)
+    num.zero? ? (return n + 1) : n += 1
+  end
+end
+
+# optimized solution for this specific problem
+# taking advantage of the fact that the smallest
+# multiple of 1-10 is 2520
+
+def smallest_multiple(n)
+  start = n
+  t = 2520
+  num = start
+  loop do
+    (t % num).zero? ? num -= 1 : num = start
+    num.zero? ? (return t + 2520) : t += 2520
   end
 end
