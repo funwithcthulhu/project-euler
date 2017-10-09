dfdcd28249bba2385c1afa47ae318feadbede3e8
funwithcthulhu <thomasboeding@gmail.com>
2017-10-08 21:21:18 -0500

    small refactoring

--- ruby/47_distinct_primef.rb
+++ ruby/47_distinct_primef.rb
@@ -9,8 +9,17 @@ def pfactor(x)
 end
 
 def find(limit)
-  (10_000..limit).map do |y|
+  a = (10_000..limit).select do |y|
     y if pfactor(y) && (pfactor(y - 1) || pfactor(y + 1))
   end
-  .compact
+  #(0..a.size - 3).each do |i|
+  a.each_index do |i|
+    #if i > a.size - 3
+    #  return 'No match found'
+      #break
+    #end
+    if (a[i] == a[i + 1] - 1) && (a[i] == a[i + 2] - 2) && (a[i] == a[i + 3] - 3)
+      return [a[i], a[i + 1], a[i + 2], a[i + 3]]
+    end
+  end
 end
