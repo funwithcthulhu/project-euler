7fe6dbc4e0cca98f99c30c1d260830c583b9c3b6
funwithcthulhu <thomasboeding@gmail.com>
2017-10-15 15:39:07 -0500

    housekeeping

--- ruby/63_powerful_dig.rb
+++ ruby/63_powerful_dig.rb
@@ -2,6 +2,14 @@
 # Similarly, the 9-digit number, 134217728=89, is a ninth power.
 # How many n-digit positive integers exist which are also an nth power?
 
+#  I initially iterated from 0..1_000 in the outside loop,
+# and 1.. 10 on the inside loop. Figured out quick from
+# the printed output that n can't be greater than 9, and narrowed
+# the possible values of the exponent down as well, all through
+# trial and error. Project Euler kept telling me my answer was
+# wrong, and I couldn't figure out why, until I realized I was
+# counting 0^1 in my tally.
+
 count = 0
 (1..9).each do |x|
   (1..25).each do |y|
