9aae0c925236d1c6993bd505edc3c460ec342df0
funwithcthulhu <thomasboeding@gmail.com>
2017-10-21 20:05:58 -0500

    memoization in 95

--- ruby/095_amicable_chains.rb
+++ ruby/095_amicable_chains.rb
@@ -1,3 +1,5 @@
+# Some memoization cuts time off 100k upper limit by 10 seconds...blech
+
 require 'benchmark'
 
 def divisors(number)
@@ -10,21 +12,40 @@ def divisors(number)
   end.sort.delete_if { |x| x == number }
 end
 
-def amicable_chain(number, sstart, n = 1)
-  return nil if number < 2
-  return nil if n > 30
-  return nil if number > 1_000_000
+Cache = {}
+
+def amicable_chain(number, sstart, n = 1, arr = [])
+  arr << number
+  if Cache.key?(number)
+    return Cache[number]
+  end
+  if number < 2
+    Cache[sstart] = 0
+    return 0
+  end
+  if n > 30
+    Cache[sstart] = 0
+    return 0
+  end
+  if number > 1_000_000
+    Cache[sstart] = 0
+    return 0
+  end
   possible = divisors(number).sum
   return n if possible == sstart && n > 1
   n += 1
-  amicable_chain(possible, sstart, n)
+  amicable_chain(possible, sstart, n, arr)
 end
 
 def search(low, lim)
   count = 0
   number = 0
   (low..lim).each do |candidate|
-    n = amicable_chain(candidate, candidate) || 0
+    if Cache.key?(candidate)
+      n = Cache[candidate]
+    else
+      n = amicable_chain(candidate, candidate, 1)
+    end
     if n > count
       count = n
       number = candidate
@@ -34,7 +55,7 @@ def search(low, lim)
 end
 
 Benchmark.bm do |x|
-  x.report { search(100, 20_000)}
+  x.report { search(100, 100_000)}
 end
 # irb(main):080:0> search(20_000)
 # longest chain: 5, least member: 12496
