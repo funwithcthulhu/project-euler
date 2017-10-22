3806cbe60ecccfe983444dccac9887fe1acbdf16
funwithcthulhu <thomasboeding@gmail.com>
2017-10-22 18:28:33 -0500

    429!!

--- ruby/429_unitary.cr
+++ ruby/429_unitary.cr
@@ -1,16 +1,6 @@
 require "benchmark"
 require "big/big_int.cr"
 
-def power(n : BigInt, exponent : BigInt)
-  prod = BigInt.new("1")
-  while exponent > 0
-    prod *= n if (exponent & 1) != 0
-    n *= n
-    exponent >>= 1
-  end
-  prod
-end
-
 def isPrime(n : Int64)
   if n == 1_i64
     false
@@ -39,6 +29,7 @@ def unitary_sum(num : Int64)
   sum = BigInt.new("1")
   factors.each do |sub_array|
     sum *= BigInt.new("1") + sub_array
+    sum %= 1_000_000_009
   end
   sum
 end
@@ -62,16 +53,5 @@ def factors(n : Int64)
 end
 
 Benchmark.bm do |x|
-  x.report("4: ") { puts unitary_sum(4_i64) }
-  x.report("100: ") { puts unitary_sum(100_i64) % 1009 }
-  x.report("1_000: ") { puts unitary_sum(1_000_i64) % 10_009 }
-  x.report("10_000: ") { puts unitary_sum(10_000_i64) % 100_009 }
-  x.report("100_000: ") { puts unitary_sum(100_000_i64) % 1_000_009 }
-  #x.report("1_000_000: ") { puts unitary_sum(1_000_000_i64) % 10_000_009 }
-  #x.report("10_000_000: ") { puts unitary_sum(10_000_000_i64) % 100_000_009 }
-  x.report("100_000_000: ") { puts unitary_sum(100_000_000_i64) % 1_000_000_009 }
+  x.report("#{ARGV[0]}") { puts unitary_sum(ARGV[0].to_i64) }
 end
-
-
-
-#####################
--- ruby/429_unitary_divisors_squares.rb
+++ ruby/429_unitary_divisors_squares.rb
@@ -1,23 +1,12 @@
 require 'prime'
 require 'benchmark'
 
-def power(n, exponent)
-  prod = 1
-  while exponent > 0
-    prod *= n if (exponent & 1) != 0
-    n *= n
-    exponent >>= 1
-  end
-  prod
-end
-
 def unitary_sum(num)
-  #factors = Prime.prime_division(num) .. finds for num
-  factors = factors(num) # finds for num!
+  factors = factors(num)
   sum = 1
-  factors.each do |sub_array|
-    #sum *= (1 + (sub_array[0]**2)**sub_array[1])
-    sum *= (1 + sub_array)
+  factors.each do |item|
+    sum *= (1 + item)
+    sum %= 1_000_000_009
   end
   sum
 end
@@ -32,19 +21,13 @@ def factors(n)
         count += (n / x**i)
         i += 1
       end
-      #primes << [x, count]
-      #primes << (x**2)**count
-      primes << power((x**2),count)
+      primes << (x**2)**count
     end
   end
   primes
 end
 
 Benchmark.bm do |x|
-  x.report { puts unitary_sum(100) }
-  x.report { puts unitary_sum(1_000) }
-  x.report { puts unitary_sum(10_000) }
-  x.report { puts unitary_sum(100_000) }
-  #x.report { puts unitary_sum(1_000_000) }
+  x.report { puts unitary_sum(ARGV[0].to_i) }
 end
 
