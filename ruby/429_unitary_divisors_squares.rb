f2077a18364c1f0608a43a87fb3a772248e54d0f
funwithcthulhu <thomasboeding@gmail.com>
2017-10-22 19:12:38 -0500

    429 edits

--- ruby/429_unitary_divisors_squares.rb
+++ ruby/429_unitary_divisors_squares.rb
@@ -1,10 +1,10 @@
 require 'prime'
 require 'benchmark'
+require 'openssl'
 
 def unitary_sum(num)
-  factors = factors(num)
   sum = 1
-  factors.each do |item|
+  factors(num).each do |item|
     sum *= (1 + item)
     sum %= 1_000_000_009
   end
@@ -13,16 +13,15 @@ end
 
 def factors(n)
   primes = []
-  (2..n).each do |x|
+  Prime.each do |x|
+    break if x > n
     count = 0
-    if x.prime?
-      i = 1
-      until x**i > n
-        count += (n / x**i)
-        i += 1
-      end
-      primes << (x**2)**count
+    i = 1
+    until x**i > n
+      count += (n / x**i)
+      i += 1
     end
+    primes << (x * x).to_bn.mod_exp(count, 1_000_000_009)
   end
   primes
 end
@@ -30,4 +29,8 @@ end
 Benchmark.bm do |x|
   x.report { puts unitary_sum(ARGV[0].to_i) }
 end
+
+#        user     system      total        real
+# 98792821
+# 41.150000   0.870000  42.020000 ( 42.207820)
 
