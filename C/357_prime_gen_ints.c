c46a6fded8b8b7f718d139de1154192f40ef5940
funwithcthulhu <thomasboeding@gmail.com>
2017-11-03 18:17:55 -0500

    format

--- C/357_prime_gen_ints.c
+++ C/357_prime_gen_ints.c
@@ -132,40 +132,40 @@ int isPrime(long n)
 {
     if (n == 1)
     {
-		return 0;
+        return 0;
     }
     else if (n < 4)
     {
-		return 1;
+        return 1;
     }
     else if (n % 2 == 0)
     {
-		return 0;
+        return 0;
     }
     else if (n < 9)
     {
-		return 1;
+        return 1;
     }
     else if (n % 3 == 0)
     {
-		return 0;
+        return 0;
     }
     else
     {
-		long r = floor(sqrt(n));
-		long f = 5;
-		while (f <= r)
-		{
-			if (n % f == 0)
-			{
-				return 0;
-			}
-			else if (n % (f + 2) == 0)
-			{
-				return 0;
-			}
-			f += 6;
-		}
-		return 1;
-	}
+        long r = floor(sqrt(n));
+        long f = 5;
+        while (f <= r)
+        {
+            if (n % f == 0)
+            {       
+                return 0;
+            }
+            else if (n % (f + 2) == 0)
+            {
+                return 0;
+            }
+            f += 6;
+        }
+        return 1;
+    }
 }
