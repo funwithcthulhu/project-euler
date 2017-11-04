05144c0c0ec530d65f2639a09cc0da4897a62222
funwithcthulhu <thomasboeding@gmail.com>
2017-11-04 12:07:33 -0500

    optimized 112

--- ruby/112_bouncy.cr
+++ ruby/112_bouncy.cr
@@ -1,3 +1,23 @@
+# Project Euler Problem 112: Bouncy Numbers
+#
+# Working from left-to-right if no digit is exceeded by the digit to its left
+# it is called an increasing number; for example, 134468.
+
+# Similarly if no digit is exceeded by the digit to its right it is called a
+# decreasing number; for example, 66420.
+
+# We shall call a positive integer that is neither increasing nor decreasing a
+# "bouncy" number; for example, 155349.
+
+# Clearly there cannot be any bouncy numbers below one-hundred, but just over half
+# of the numbers below one-thousand (525) are bouncy. In fact, the least number for
+# which the proportion of bouncy numbers first reaches 50% is 538.
+
+# Surprisingly, bouncy numbers become more and more common and by the time we reach
+# 21780 the proportion of bouncy numbers is equal to 90%.
+
+# Find the least number for which the proportion of bouncy numbers is exactly 99%.
+
 require "benchmark"
 
 def incr(number)
@@ -15,13 +35,17 @@ def tab(proportion : Float64)
   bouncy = 0
   last = 0
   (1..Float64::INFINITY).each do |candidate|
-    non += 1 if incr(candidate) || decr(candidate)
-    bouncy += 1 if !incr(candidate) && !decr(candidate)
+    if incr(candidate) || decr(candidate)
+      non += 1
+    else
+      bouncy += 1
+    end
     last = candidate
     break if bouncy.to_f / (non.to_f + bouncy.to_f) == proportion
   end
   puts last
 end
+
 
 Benchmark.bm do |x|
   x.report("50%: ") { tab(0.5_f64) }
@@ -31,8 +55,8 @@ end
 
 # user     system      total        real
 # 50%:  538
-# 0.000000   0.000000   0.000000 (  0.002391)
+#  0.000000   0.000000   0.000000 (  0.003447)
 # 90%:  21780
-# 0.070000   0.020000   0.090000 (  0.072222)
+#  0.030000   0.010000   0.040000 (  0.037757)
 # 99%:  1587000
-# 5.300000   1.540000   6.840000 (  5.795595)
+#  2.560000   0.820000   3.380000 (  2.747045)
--- ruby/112_bouncy_numbers.rb
+++ ruby/112_bouncy_numbers.rb
@@ -1,3 +1,23 @@
+# Project Euler Problem 112: Bouncy Numbers
+#
+# Working from left-to-right if no digit is exceeded by the digit to its left
+# it is called an increasing number; for example, 134468.
+
+# Similarly if no digit is exceeded by the digit to its right it is called a
+# decreasing number; for example, 66420.
+
+# We shall call a positive integer that is neither increasing nor decreasing a
+# "bouncy" number; for example, 155349.
+
+# Clearly there cannot be any bouncy numbers below one-hundred, but just over half
+# of the numbers below one-thousand (525) are bouncy. In fact, the least number for
+# which the proportion of bouncy numbers first reaches 50% is 538.
+
+# Surprisingly, bouncy numbers become more and more common and by the time we reach
+# 21780 the proportion of bouncy numbers is equal to 90%.
+
+# Find the least number for which the proportion of bouncy numbers is exactly 99%.
+
 require 'benchmark'
 
 def incr(number)
@@ -13,14 +33,17 @@ end
 def tab(proportion)
   non = 0
   bouncy = 0
-  @last = 0
+  last = 0
   (1..Float::INFINITY).each do |candidate|
-    non += 1 if incr(candidate) || decr(candidate)
-    bouncy += 1 if !incr(candidate) && !decr(candidate)
-    @last = candidate
+    if incr(candidate) || decr(candidate)
+      non += 1 
+    else
+      bouncy += 1
+    end
+    last = candidate
     break if bouncy.to_f / (non.to_f + bouncy.to_f) == proportion
   end
-  puts @last
+  puts last
 end
 
 Benchmark.bm do |x|
@@ -31,8 +54,8 @@ end
 
 # user     system      total        real
 # 50%: 538
-# 0.010000   0.000000   0.010000 (  0.007629)
+#  0.010000   0.000000   0.010000 (  0.003865)
 # 90%: 21780
-# 0.450000   0.000000   0.450000 (  0.451168)
+#  0.220000   0.000000   0.220000 (  0.231804)
 # 99%: 1587000
-# 41.130000   0.190000  41.320000 ( 41.605658)
+# 20.450000   0.070000  20.520000 ( 20.608861)
